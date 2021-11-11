a791a2b49f2bb0a73d92313fd2aecf63a9edc7b6
javier cuasapud <edwarcuasapud@gmail.com>
2021-11-11 12:57:39 -0500

    Api rest funcionando CRUD de usuarios

--- src/main/java/com/javierfullstack/app/controller/UserController.java
+++ src/main/java/com/javierfullstack/app/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.javierfullstack.app.controller;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +44,40 @@ public class UserController {
 	}
 	return ResponseEntity.ok(oUser);
 	}
+	//update user
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update (@RequestBody User userDetails, @PathVariable (value = "id") Long userId){
+		Optional<User> user = userService.findById(userId);
+		if(!user.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		//
+		user.get().setName(userDetails.getName());
+		user.get().setSurname(userDetails.getSurname());
+		user.get().setEmail(userDetails.getEmail());
+		user.get().setEnabled(userDetails.getEnabled());
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user.get()));
 	}
+	
+	//delete an user
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete (@PathVariable(value = "id") Long userId){
+		 if (!userService.findById(userId).isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		 userService.deleteById(userId);
+		 return ResponseEntity.ok().build();
+	}
+	// Read all users
+	@GetMapping
+	public List<User> readAll () {
+		List<User> users =StreamSupport.
+				stream(userService.findAll().spliterator(), false).collect(Collectors.toList());
+		return users;
+	}
+
+}
+	
+
 
